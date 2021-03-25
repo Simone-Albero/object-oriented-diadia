0aee3b98c33bb7276645f0c3068e80260509a5fe
Simone <simone.albero29@gmail.com>
2021-03-25 18:55:06 +0100

    stato embrionale

---
+++ diadia/src/StanzaTest1.java
@@ -0,0 +1,17 @@
+import it.uniroma3.diadia.Stanza;
+
+public class StanzaTest1 {
+
+	public static void main(String[] args) {
+		Stanza bar= new Stanza("bar");
+		Stanza mensa= new Stanza("mensa");
+		
+		bar.impostaStanzaAdiacente("nord", mensa);
+		mensa.impostaStanzaAdiacente("sud", bar);
+		
+		System.out.println("Porta nord bar --> " + bar.getStanzaAdiacente("nord"));
+		System.out.println("Porta sud mensa --> " + mensa.getStanzaAdiacente("sud"));
+
+	}
+
+}

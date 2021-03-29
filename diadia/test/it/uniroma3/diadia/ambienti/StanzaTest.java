348c75f6ce120fe23410a00395b884f56899bf42
Simone <simone.albero29@gmail.com>
2021-03-29 19:35:51 +0200

    modifiche al metodo test Stanza
    minimizzato il codice

--- diadia/test/it/uniroma3/diadia/ambienti/StanzaTest.java
+++ diadia/test/it/uniroma3/diadia/ambienti/StanzaTest.java
@@ -3,57 +3,57 @@ package it.uniroma3.diadia.ambienti;
 import it.uniroma3.diadia.attrezzi.Attrezzo;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StanzaTest {
 	
-	private Stanza[] stanzeAdiacenti(String s1, String s2, String direzione) {
-		Stanza stanze[] = {new Stanza(s1), new Stanza(s2)};
-		stanze[0].impostaStanzaAdiacente(direzione, stanze[1]);
-		return stanze;
+	private Stanza stanze[];
+	private Stanza stanza;
+	private Attrezzo attrezo;
+	
+	@Before
+	public void setUp() {
+		this.stanze = new Stanza[2];
+		this.stanze[0] = new Stanza("atrio"); 
+		this.stanze[1] = new Stanza("biblioteca");
+		this.stanze[0].impostaStanzaAdiacente("nord", stanze[1]);
+		
+		this.stanza = new Stanza("atrio");
+		this.attrezo = new Attrezzo("osso", 10); 
+		stanza.addAttrezzo(this.attrezo); 
+		
 	}
 	
 	
 	@Test
-	public void testGetStanzaAdiacenteNonNulla() {
-		Stanza stanza[] = stanzeAdiacenti("atrio", "biblioteca", "nord"); 
-		assertEquals(stanza[1], stanza[0].getStanzaAdiacente("nord"));
+	public void testGetStanzaAdiacenteNonNulla() { 
+		assertEquals(this.stanze[1], this.stanze[0].getStanzaAdiacente("nord"));
 	}
 	
 	@Test
 	public void testGetStanzaAdiacenteNulla() {
-		Stanza stanza[] = stanzeAdiacenti("atrio", "biblioteca", "nord"); 
-		assertEquals(null, stanza[0].getStanzaAdiacente("sud"));
+		assertEquals(null, this.stanze[0].getStanzaAdiacente("sud"));
 	}
 	
 	@Test
 	public void testGetStanzaAdiacenteDirezioneInesistente() {
-		Stanza stanza[] = stanzeAdiacenti("atrio", "biblioteca", "nord"); 
-		assertEquals(null, stanza[0].getStanzaAdiacente("pippo"));
+		assertEquals(null, this.stanze[0].getStanzaAdiacente("pippo"));
 	}
 	
 	@Test
 	public void testGetAttrezzoNonNullo() {
-		Stanza stanza = new Stanza("atrio");
-		Attrezzo a = new Attrezzo("osso", 10); 
-		stanza.addAttrezzo(a); 
-		assertEquals(a, stanza.getAttrezzo("osso"));
+		assertEquals(this.attrezo , this.stanza.getAttrezzo("osso"));
 	}
 	
 	@Test
 	public void testGetAttrezzoNullo() {
-		Stanza stanza = new Stanza("atrio");
-		Attrezzo a = new Attrezzo("osso", 10); 
-		stanza.addAttrezzo(a); 
-		assertEquals(null, stanza.getAttrezzo(null));
+		assertEquals(null , this.stanza.getAttrezzo(null));
 	}
 	
 	@Test
 	public void testGetAttrezzoInesistente() {
-		Stanza stanza = new Stanza("atrio");
-		Attrezzo a = new Attrezzo("osso", 10); 
-		stanza.addAttrezzo(a); 
-		assertEquals(null, stanza.getAttrezzo("pippo"));
+		assertEquals(null , this.stanza.getAttrezzo("pippo"));
 	}
 
 }
